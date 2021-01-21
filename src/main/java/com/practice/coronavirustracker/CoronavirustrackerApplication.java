1d5bab2b04781d943ac4fdebbc50924b5a07fd07
Alka Raghuvanshi <alka.abhi66@gmail.com>
2021-01-22 02:34:07 +0530

    Initial commit

---
+++ src/main/java/com/practice/coronavirustracker/CoronavirustrackerApplication.java
@@ -0,0 +1,17 @@
+package com.practice.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class CoronavirustrackerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CoronavirustrackerApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
