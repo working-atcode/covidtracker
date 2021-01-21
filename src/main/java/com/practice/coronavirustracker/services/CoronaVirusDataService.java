37dde7e1f75d229214b1ef56a707a734760bbb27
Alka Raghuvanshi <alka.abhi66@gmail.com>
2021-01-22 04:26:23 +0530

    Add csv reader to read from csv file for specific column

--- src/main/java/com/practice/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/practice/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,9 +1,13 @@
 package com.practice.coronavirustracker.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -21,6 +25,11 @@ public class CoronaVirusDataService {
                 .uri(URI.create(VIRUS_DATA_URL))
                 .build();
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+            System.out.println(state);
+        }
     }
 }
